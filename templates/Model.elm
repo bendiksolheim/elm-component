5923905027fc6899494ce542b512d121a3e2f39e
Bendik Solheim <bendik@solheim.im>
2016-12-04 18:05:08 +0100

    Make initModel only return Model, not Cmd

--- templates/Model.elm
+++ templates/Model.elm
@@ -2,12 +2,13 @@ module %s.Model exposing (Model, initModel)
 
 import %s.Messages exposing (..)
 
+
 type alias Model =
     { entry : String
     }
 
 
-initModel : (Model, Cmd Msg)
+initModel : Model
 initModel =
-    (Model "", Cmd.none)
+    Model ""
 
